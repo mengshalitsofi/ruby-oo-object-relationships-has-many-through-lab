e158f13ea1152c5943be68e47953318c8d3b64f1
mengshalitsofi <mengshalitsofi@gmail.com>
2021-01-27 20:15:06 -0800

    commit tree d8ecbef3ba6f43357f1b8192edf37373d43b67b1

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,19 @@
+class Appointment
 
+    @@all = []
+    
+    attr_reader :patient
+    attr_reader :doctor    
+
+    def initialize(date, patient, doctor)
+        @@all << self
+        @date = date
+        @patient = patient
+        @doctor = doctor
+    end
+
+    def self.all
+        @@all
+    end
+
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,19 @@
+class Song
+
+    @@all = []
+    attr_reader :artist
+    attr_reader :genre
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
 
